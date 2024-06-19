9e30a6bedc12996a8e7ae2077c1dc9ce2f82cf48
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-06-19 18:48:22 +0200

    added some interaction

--- lollms/server/endpoints/lollms_file_system.py
+++ lollms/server/endpoints/lollms_file_system.py
@@ -155,11 +155,13 @@ def select_rag_database(client) -> Optional[Dict[str, Path]]:
                             text = TextDocumentsLoader.read_file(fn)
                             title = fn.stem  # Use the file name without extension as the title
                             vdb.add_document(title, text, fn)
+                            lollmsElfServer.ShowBlockingMessage(f"Adding a new database.\nAdding {title}")
                             print(f"Added document: {title}")
                         except Exception as e:
                             print(f"Failed to add document {fn}: {e}")
                     if vdb.new_data: #New files are added, need reindexing
                         ASCIIColors.blue("Indexing database ...", end="", flush=True)
+                        lollmsElfServer.ShowBlockingMessage(f"Adding a new database.\nIndexing database")
                         vdb.build_index()
                         ASCIIColors.success("OK")
                     lollmsElfServer.HideBlockingMessage()
